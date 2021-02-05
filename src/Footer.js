8ca8c6aa4f321e9f6b088300e4ae578a92d587e4
Maxence Derepas <maxence.derepas@gmail.com>
2021-02-05 17:29:42 +0100

    change footer

--- src/Footer.js
+++ src/Footer.js
@@ -2,7 +2,7 @@ const Footer = () => {
     return (
         <footer>
             Made with <span> React </span> at <span> Le Reacteur </span> by
-            <span> Brice</span>.
+            <span> Maxence Derepas</span>.
         </footer>
     );
 };
